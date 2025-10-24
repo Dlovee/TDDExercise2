5f3a6912f6550bafe54a063bd2e6952c2cdcca01
Dlove <dlove327@gmail.com>
2025-10-24 14:03:41 -0500

    update

--- PalindromeExercise/PalindromeExercise/WordSmith.cs
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -6,12 +6,18 @@ namespace PalindromeExercise
     {
         public bool IsAPalindrome(string word)
         {
-            if (string.IsNullOrEmpty(word)) return false;
+            if (string.IsNullOrEmpty(word))
+                return false;
 
-            string lowered = word.ToLower();
-            string reversed = new string(lowered.Reverse().ToArray());
+            string wordLowered = word.ToLower();
+            string backwards = "";
 
-            return lowered == reversed;
+            for (int i = wordLowered.Length - 1; i >= 0; i--)
+            {
+                backwards += wordLowered[i];
+            }
+
+            return wordLowered == backwards;
         }
     }
 }
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,19 +1,25 @@
 using PalindromeExercise;
 using Xunit;
 
-namespace PalindromeExerciseTests;
-
-public class UnitTest1
+namespace PalindromeExerciseTests
 {
-    [Theory]
-    [InlineData("racecar", true)]
-    [InlineData("John", false)]
-    [InlineData("", false)]
-    [InlineData(null, false)]
-    public void IsAPalindromeTest(string word, bool expected)
+    public class UnitTest1
     {
-        var ws = new WordSmith();
-        var actual = ws.IsAPalindrome(word);
-        Assert.Equal(expected, actual);
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("kayak", true)]
+        [InlineData("John", false)]
+        [InlineData("Jose", false)]
+        [InlineData("Amoriss", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void IsAPalindromeTest(string word, bool expected)
+        {
+            var ws = new WordSmith();
+            var actual = ws.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
+        }
     }
 }
