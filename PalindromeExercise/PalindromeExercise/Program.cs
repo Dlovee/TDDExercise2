5359c7ec08def4d7b06caf696b7deb6ea4111b3e
Dlove <dlove327@gmail.com>
2025-10-29 16:22:11 -0500

    update

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -1,38 +1,6 @@
 ﻿using System;
 
-namespace PalindromeExercise
+void Main(string[] args)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            bool IsAPalindrome(string word)
-            {
-                if (word == null || word.Length == 0)
-                {
-                    return false;
-                }
-
-                string wordLowered = word.ToLower();
-                string backwards = "";
-
-                for (int i = wordLowered.Length - 1; i >= 0; i--)
-                {
-                    backwards += wordLowered[i];
-                }
-        
-                return wordLowered == backwards;
-
-                // if (wordLowered == backwards)
-                // {
-                //     return true;
-                // }
-                // else
-                // {
-                //     return false;
-                // }
-
-            }
-        }
-    }
+    
 }
